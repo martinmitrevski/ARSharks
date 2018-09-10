3ac69f1c01d2d2d63fd5719a2a9821fc5ad69d0f
martinmitrevski <martinmitrevski.oh@gmail.com>
2018-09-10 15:19:26 +0200

    project import

---
+++ ARAnimals/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  ARAnimals
+//
+//  Created by Martin Mitrevski on 10.09.18.
+//  Copyright © 2018 Mitrevski. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/sharks.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+}
